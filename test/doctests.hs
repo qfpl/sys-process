50cf9dedd5beee2a1035856235e181db895edae5
Tony Morris <tmorris@tmorris.net>
2015-06-25 20:40:57 +1000

    Don't always import Control.Applicative

--- test/doctests.hs
+++ test/doctests.hs
@@ -1,7 +1,11 @@
+{-# LANGUAGE CPP #-}
+
 module Main where
 
+#if !(MIN_VERSION_base(4,8,0))
+import Control.Applicative(Applicative((<*>), pure))
+#endif
 import Build_doctests (deps)
-import Control.Applicative
 import Control.Monad
 import Data.List
 import System.Directory
